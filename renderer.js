c012e4b782ee35227db375fe209ed8fead6bafe9
mervyn <feimei.zhan@live.com>
2022-06-30 16:59:48 +0800

    #41 add auto launch setting UI

--- renderer.js
+++ renderer.js
@@ -6,3 +6,11 @@ silentTimeRange.addEventListener('change', (event) => {
 
 silentTimeRange.value = window.api.getStore(window.api.STORE_KEY.silentTimeHr)
 silentTimeRange.nextElementSibling.value = `${silentTimeRange.value}h`
+
+const autoLaunch = document.querySelector('#auto-launch')
+
+autoLaunch.addEventListener('change', (event) => {
+  window.api.setStore(window.api.STORE_KEY.autoLaunch, event.target.checked)
+})
+
+autoLaunch.checked = window.api.getStore(window.api.STORE_KEY.autoLaunch, false)
