e344e00b0ee42eda7d16d1d14cdc83f2f60ee7d2
mervyn <feimei.zhan@live.com>
2022-06-30 17:24:16 +0800

    #41 finished the feature auto launch

--- tool.js
+++ tool.js
@@ -3,6 +3,11 @@ function isDev () {
   return process.env.NODE_ENV === 'development'
 }
 
+function getIpcStoreKey (key) {
+  return `store:${key}`
+}
+
 module.exports = {
-  isDev
+  isDev,
+  getIpcStoreKey
 }
