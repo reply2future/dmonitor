038db1ab754533a4ffb7f09b742726fb8744d61c
mervyn <feimei.zhan@live.com>
2022-07-03 15:15:56 +0800

    #33 refact menu class

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const path = require('path')
-const { Monitor, ACTION_EVENT, STATUS_EVENT } = require('./monitor')
-const { app, Menu, Notification, Tray, nativeImage, BrowserWindow, ipcMain, shell } = require('electron')
+const { Monitor, ACTION_EVENT } = require('./monitor')
+const { app, Notification, nativeImage, ipcMain } = require('electron')
 const { isDev, getIpcStoreKey } = require('./tool')
 const { store, STORE_KEY } = require('./store')
+const { MenuTray, MENU_CLICK_EVENT } = require('./menu')
 const logger = require('electron-log')
 
 const monitorConfig = isDev() ? { windowSize: 5 } : {}
@@ -10,12 +11,7 @@ const monitor = new Monitor(monitorConfig)
 
 monitor.on(ACTION_EVENT.REMOVE, ({ pid }) => {
   logger.warn(`pid: ${pid} becomes normal`)
-  const statusItem = cachedMenus.find(item => item.id === STATUS_ID)
-  cachedMenus = cachedMenus.filter(item => item.id !== pid)
-  const hasPid = cachedMenus.some(item => item.isPid)
-  if (!hasPid) statusItem.label = STATUS_NO_PID_LABEL
-
-  menuTray.setContextMenu(Menu.buildFromTemplate(cachedMenus))
+  menuTray.removePid({ pid })
 })
 
 monitor.on(ACTION_EVENT.ADD, ({ pid, stat }) => {
@@ -23,140 +19,28 @@ monitor.on(ACTION_EVENT.ADD, ({ pid, stat }) => {
   logger.warn(`pid: ${pid}, command: ${stat.command} drains the battery fast`)
   new Notification({ title: 'ooops', body: `The command "${command}" is draining the battery fast and pid is ${pid}` }).show()
 
-  const statusItem = cachedMenus.find(item => item.id === STATUS_ID)
-  statusItem.label = STATUS_HAS_PID_LABEL
-
-  cachedMenus.push({
-    label: `kill ${command} (${pid})`,
-    toolTip: `the path is "${stat.command}"`,
-    type: 'normal',
-    isPid: true,
-    id: pid,
-    after: [STATUS_ID],
-    click: () => {
+  menuTray.addPid({
+    pid,
+    stat,
+    clickFn: () => {
       process.kill(pid)
       logger.info(`sent SIGTERM to command ${stat.command} pid ${pid}`)
-      cachedMenus = cachedMenus.filter(item => item.id !== pid)
-      const hasPid = cachedMenus.some(item => item.isPid)
-      if (!hasPid) statusItem.label = STATUS_NO_PID_LABEL
-      menuTray.setContextMenu(Menu.buildFromTemplate(cachedMenus))
     }
-  })
-  menuTray.setContextMenu(Menu.buildFromTemplate(cachedMenus))
-})
-
-Object.entries(STATUS_EVENT).forEach(([_, value]) => {
-  monitor.on(value, () => {
-    updateStatusItem(value)
   })
 })
 
-function updateStatusItem (value) {
-  const isRunning = value === STATUS_EVENT.START
-  const radios = cachedMenus.filter(item => item.type === 'radio')
-  radios.forEach(item => {
-    if ((isRunning && item.id === ON_ID) || (!isRunning && item.id === OFF_ID)) {
-      item.checked = true
-    } else {
-      delete item.checked
-    }
-  })
-}
-
-let cachedMenus
 let menuTray
-
-const STATUS_ID = 'status'
-const ON_ID = 'on'
-const OFF_ID = 'off'
-const SILENT_ID = 'silent'
-const STATUS_NO_PID_LABEL = 'There is no draining process'
-const STATUS_HAS_PID_LABEL = 'There are some processes draining the battery'
-const DEFAULT_SILENT_TIME_HR = 1
-
-let silentTimeoutId = null
-
-function generateSilentTimeLabel () {
-  return `    silent mode(${store.get(STORE_KEY.silentTimeHr, 1)} hour)`
-}
 
 function initMenuBar () {
   const icon = nativeImage.createFromPath(path.join(__dirname, 'asserts', 'BlackIconTemplate.png'))
-  menuTray = new Tray(icon)
-
-  cachedMenus = [
-    {
-      label: 'Current Status',
-      enabled: false
-    },
-    {
-      label: '    on',
-      id: ON_ID,
-      accelerator: 'CmdOrCtrl+S',
-      type: 'radio',
-      click: () => {
-        clearTimeout(silentTimeoutId)
-        monitor.start()
-      }
-    },
-    {
-      label: '    off',
-      id: OFF_ID,
-      type: 'radio',
-      checked: true,
-      accelerator: 'CmdOrCtrl+P',
-      click: () => {
-        clearTimeout(silentTimeoutId)
-        monitor.stop()
-      }
-    },
-    {
-      label: generateSilentTimeLabel(),
-      id: SILENT_ID,
-      type: 'radio',
-      accelerator: 'CmdOrCtrl+L',
-      click: () => {
-        clearTimeout(silentTimeoutId)
-        monitor.stop()
-        silentTimeoutId = setTimeout(() => {
-          monitor.start()
-          cachedMenus.filter(item => item.type === 'radio').forEach(item => {
-            if (item.id === ON_ID) item.checked = true
-            else delete item.checked
-          })
-          menuTray.setContextMenu(Menu.buildFromTemplate(cachedMenus))
-        }, store.get(STORE_KEY.silentTimeHr, DEFAULT_SILENT_TIME_HR) * 3600 * 1000)
-      }
-    },
-    { type: 'separator' },
-    { label: STATUS_NO_PID_LABEL, type: 'normal', id: STATUS_ID },
-    { type: 'separator' },
-    {
-      label: 'Preferences',
-      click: () => {
-        const win = new BrowserWindow({
-          width: 800,
-          height: 600,
-          webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-          }
-        })
-        win.loadFile('preference.html')
-        win.on('close', () => {
-          const silentItem = cachedMenus.find(item => item.id === SILENT_ID)
-          silentItem.label = generateSilentTimeLabel()
-          menuTray.setContextMenu(Menu.buildFromTemplate(cachedMenus))
-        })
-      }
-    },
-    { label: 'Help', role: 'help', click: () => shell.openExternal('https://github.com/reply2future/dmonitor/issues') },
-    { type: 'separator' },
-    { label: 'Quit', role: 'quit' },
-    { type: 'separator' }
-  ]
-
-  menuTray.setToolTip('dmonitor!')
-  menuTray.setContextMenu(Menu.buildFromTemplate(cachedMenus))
+  menuTray = new MenuTray(icon)
+  menuTray.on(MENU_CLICK_EVENT.ON, () => {
+    monitor.start()
+  })
+  menuTray.on(MENU_CLICK_EVENT.OFF, () => {
+    monitor.stop()
+  })
+  menuTray.updateUi()
 }
 
 function initListeners () {
---
+++ menu.js
@@ -0,0 +1,148 @@
+const { BrowserWindow, Menu, shell, Tray } = require('electron')
+const { EventEmitter } = require('events')
+const { store, STORE_KEY } = require('./store')
+const path = require('path')
+
+const STATUS_ID = 'status'
+const ON_ID = 'on'
+const OFF_ID = 'off'
+const SILENT_ID = 'silent'
+const STATUS_NO_PID_LABEL = 'There is no draining process'
+const STATUS_HAS_PID_LABEL = 'There are some processes draining the battery'
+const ISSUES_URL = 'https://github.com/reply2future/dmonitor/issues'
+const DEFAULT_SILENT_TIME_HR = 1
+
+const MENU_CLICK_EVENT = {
+  ON: 'on',
+  OFF: 'off',
+  SILENT: 'silent'
+}
+
+class MenuTray extends EventEmitter {
+  constructor (icon) {
+    super()
+    this.tray = new Tray(icon)
+    this.silentTimeoutId = null
+    this.items = [
+      {
+        label: 'Current Status',
+        enabled: false
+      },
+      {
+        label: '    on',
+        id: ON_ID,
+        accelerator: 'CmdOrCtrl+S',
+        type: 'radio',
+        click: () => {
+          clearTimeout(this.silentTimeoutId)
+          this.updateStatusItem(MENU_CLICK_EVENT.ON)
+          this.emit(MENU_CLICK_EVENT.ON)
+        }
+      },
+      {
+        label: '    off',
+        id: OFF_ID,
+        type: 'radio',
+        checked: true,
+        accelerator: 'CmdOrCtrl+P',
+        click: () => {
+          clearTimeout(this.silentTimeoutId)
+          this.updateStatusItem(MENU_CLICK_EVENT.OFF)
+          this.emit(MENU_CLICK_EVENT.OFF)
+        }
+      },
+      {
+        label: this.generateSilentTimeLabel(),
+        id: SILENT_ID,
+        type: 'radio',
+        accelerator: 'CmdOrCtrl+L',
+        click: () => {
+          clearTimeout(this.silentTimeoutId)
+          this.updateStatusItem(MENU_CLICK_EVENT.OFF)
+          this.emit(MENU_CLICK_EVENT.OFF)
+          this.silentTimeoutId = setTimeout(() => {
+            this.updateStatusItem(MENU_CLICK_EVENT.ON)
+            this.emit(MENU_CLICK_EVENT.ON)
+          }, store.get(STORE_KEY.silentTimeHr, DEFAULT_SILENT_TIME_HR) * 3600 * 1000)
+        }
+      },
+      { type: 'separator' },
+      { label: STATUS_NO_PID_LABEL, type: 'normal', id: STATUS_ID },
+      { type: 'separator' },
+      {
+        label: 'Preferences',
+        click: () => {
+          const win = new BrowserWindow({
+            width: 800,
+            height: 600,
+            webPreferences: {
+              preload: path.join(__dirname, 'preload.js')
+            }
+          })
+          win.loadFile('preference.html')
+          win.on('close', () => {
+            const silentItem = this.items.find(item => item.id === SILENT_ID)
+            silentItem.label = this.generateSilentTimeLabel()
+            this.updateUi()
+          })
+        }
+      },
+      { label: 'Help', role: 'help', click: () => shell.openExternal(ISSUES_URL) },
+      { type: 'separator' },
+      { label: 'Quit', role: 'quit' },
+      { type: 'separator' }
+    ]
+  }
+
+  updateStatusItem (value) {
+    const isRunning = value === MENU_CLICK_EVENT.ON
+    this.items
+      .filter(item => item.type === 'radio')
+      .forEach(item => {
+        item.checked = (isRunning && item.id === ON_ID) || (!isRunning && item.id === OFF_ID)
+      })
+    this.updateUi()
+  }
+
+  generateSilentTimeLabel () {
+    return `    silent mode(${store.get(STORE_KEY.silentTimeHr, 1)} hour)`
+  }
+
+  addPid ({ pid, stat, clickFn }) {
+    const command = path.basename(stat.command)
+    const statusItem = this.items.find(item => item.id === STATUS_ID)
+    statusItem.label = STATUS_HAS_PID_LABEL
+
+    this.items.push({
+      label: `kill ${command} (${pid})`,
+      toolTip: `the path is "${stat.command}"`,
+      type: 'normal',
+      isPid: true,
+      id: pid,
+      after: [STATUS_ID],
+      click: () => {
+        clickFn()
+        this.items = this.items.filter(item => item.id !== pid)
+        const hasPid = this.items.some(item => item.isPid)
+        if (!hasPid) statusItem.label = STATUS_NO_PID_LABEL
+        this.updateUi()
+      }
+    })
+    this.updateUi()
+  }
+
+  removePid ({ pid }) {
+    const statusItem = this.items.find(item => item.id === STATUS_ID)
+    this.items = this.items.filter(item => item.id !== pid)
+    const hasPid = this.items.some(item => item.isPid)
+    if (!hasPid) statusItem.label = STATUS_NO_PID_LABEL
+
+    this.updateUi()
+  }
+
+  updateUi () {
+    this.tray.setContextMenu(Menu.buildFromTemplate(this.items))
+  }
+}
+
+module.exports = { MenuTray, MENU_CLICK_EVENT }
